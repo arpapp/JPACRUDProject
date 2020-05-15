f82622c8fe32b5d06d616e52bc685eb691963691
arpapp <trpapp@gmail.com>
2020-05-15 17:37:37 -0600

    connected stuff

---
+++ BarsApp/src/main/java/com/skilldistillery/bars/BarsAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.bars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarsAppApplication.class, args);
+	}
+
+}
---
+++ BarsApp/src/test/java/com/skilldistillery/bars/BarsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.bars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
